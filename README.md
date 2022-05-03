4e57baa09d2c2b4ce9ffdd88825c98fb864d03ec
LukasBommes <lukas.bommes@gmx.de>
2022-05-03 16:32:13 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+### Activate Python Virtual Environment
+
+source env/bin/activate
+
+### Run development server
+
+Activate virtual environment.
+
+```
+Grid-Annotation-Tool-v3$ uvicorn api.main:app --reload
+```
+
+### Run Tests
+
+```
+Grid-Annotation-Tool-v3/api/tests$ pytest
+```
--- api/models.py
+++ api/models.py
@@ -21,6 +21,7 @@ class Image(Base):
     __tablename__ = "images"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
+    #filepath = Column
     project_id = Column(Integer, ForeignKey("projects.id"))
 
     project = relationship("Project", back_populates="images")
