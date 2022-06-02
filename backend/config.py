7ba5e3a2e0eb3d9364e99baed382d62bf5c731e1
LukasBommes <lukas.bommes@gmx.de>
2022-06-02 16:41:03 +0200

    before modifying editor

--- backend/config.py
+++ backend/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str = "sqlite:///./api.db"  #"postgresql://user:password@postgresserver/db"
     SECRET_KEY = "cf568d8fee94fdc3d3ec1482db922643cf708158cc2347988a564223d3db5f3b"
     ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 1 # 1 for testing
+    ACCESS_TOKEN_EXPIRE_MINUTES = 120 # 1 for testing
 
 
 settings = Settings()
