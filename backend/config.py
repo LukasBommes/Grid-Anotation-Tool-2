9363a0944c6aa2e6b200b8ff48f2d460bcc94622
LukasBommes <lukas.bommes@gmx.de>
2022-06-02 15:47:07 +0200

    fixed authentication problem when getting images

--- backend/config.py
+++ backend/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str = "sqlite:///./api.db"  #"postgresql://user:password@postgresserver/db"
     SECRET_KEY = "cf568d8fee94fdc3d3ec1482db922643cf708158cc2347988a564223d3db5f3b"
     ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 1 #30
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30 # 1 for testing
 
 
 settings = Settings()
