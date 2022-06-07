d0c0f62b0df5173fcb649739494568e6f6c2910a
LukasBommes <lukas.bommes@gmx.de>
2022-06-07 14:57:02 +0200

    fixed error with non persistent volume (path was wrong)

--- backend/config.py
+++ backend/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
         "http://localhost",
         "http://127.0.0.1",
     ]
-    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./api.db")  #"postgresql://user:password@postgresserver/db"
+    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./api.db")
     SECRET_KEY = os.getenv("SECRET_KEY", "cf568d8fee94fdc3d3ec1482db922643cf708158cc2347988a564223d3db5f3b")
     ALGORITHM = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 120)) # 1 for testing
