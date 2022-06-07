b52c313bdc53a7dc64759cfd5457489efc15ebfb
LukasBommes <lukas.bommes@gmx.de>
2022-06-07 17:28:24 +0200

    using docker secrets now

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     MEDIA_ROOT: str = os.getenv("BACKEND_MEDIA_ROOT", "images")
     CORS_ORIGINS: List[str] = json.loads(os.getenv("BACKEND_CORS_ORIGINS", '[]'))
     DATABASE_URL: str = os.getenv("BACKEND_DATABASE_URL", "sqlite:///./api.db")
-    ACCESS_TOKEN_SECRET_KEY = os.getenv("BACKEND_ACCESS_TOKEN_SECRET_KEY", "cf568d8fee94fdc3d3ec1482db922643cf708158cc2347988a564223d3db5f3b")
+    ACCESS_TOKEN_SECRET_KEY = os.getenv("BACKEND_ACCESS_TOKEN_SECRET_KEY")
     ACCESS_TOKEN_ALGORITHM = os.getenv("BACKEND_ACCESS_TOKEN_ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("BACKEND_ACCESS_TOKEN_EXPIRE_MINUTES", 120)) # 1 for testing
 
