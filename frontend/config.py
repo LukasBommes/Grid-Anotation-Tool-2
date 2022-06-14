7ed64b5b81f1ccfa94d4a55a1663e1f4a86d6644
LukasBommes <lukas.bommes@gmx.de>
2022-06-14 16:44:20 +0200

    finished traefik configuration

--- frontend/config.py
+++ frontend/config.py
@@ -1,10 +1,11 @@
+import os
 from typing import List
 
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    API_URL: str = "http://localhost:8000/api"
+    API_URL: str = os.getenv("API_URL", "http://localhost:8000/api") 
 
 
 settings = Settings()
