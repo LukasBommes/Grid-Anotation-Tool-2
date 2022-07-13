6f5f426db45e7bb29a5449ee5f24a1d7822f4b28
LukasBommes <lukas.bommes@gmx.de>
2022-07-13 12:44:21 +0200

    finalized workflow

--- backend/database.py
+++ backend/database.py
@@ -8,6 +8,6 @@ print(settings.DATABASE_URL)
 engine = create_engine(
     settings.DATABASE_URL  # , connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine )
 
 Base = declarative_base()
